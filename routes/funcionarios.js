b880bc5c8b4ed8d895674b8ae53fdbb821ed4e29
Vitor Daniel Lopes dos Santos <20211144010044@ifrn.local>
2023-10-30 16:59:58 -0300

    adicionar página de cadastro de funcionário

--- routes/funcionarios.js
+++ routes/funcionarios.js
@@ -5,12 +5,12 @@ router.get('/', function(req, res, next) {
   res.render('funcionarios/listagem_funcionarios', { page: 'funcionarios' });
 });
 
-router.get('/:id', function(req, res, next) {
-  res.render('funcionarios/visualizar_funcionario', { title: "Vitor Daniel", page: 'funcionarios' });
-});
-
 router.get('/cadastrar', function(req, res, next) {
   res.render('funcionarios/cadastrar_funcionario', { page: 'funcionarios' });
+});
+
+router.get('/:id', function(req, res, next) {
+  res.render('funcionarios/visualizar_funcionario', { title: "Vitor Daniel", page: 'funcionarios' });
 });
 
 router.get('/:id/cadastrar_dependente', function(req, res, next) {
