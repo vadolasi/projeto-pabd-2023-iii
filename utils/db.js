a7b41277d8e0e57c81307d04e6d8dbd992c79b3a
Vitor Daniel Lopes dos Santos <20211144010044@ifrn.local>
2023-10-30 17:23:13 -0300

    adiconar conexão ao mysql

---
+++ utils/db.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'dbmaquiagem',
+    multipleStatements: true
+});
+
+db.connect((erro) => {
+    if (erro){
+        throw erro;
+    }
+
+    console.log('Conectado ao BD...');
+});
+
+
+global.db = db;
+
+module.exports = db;
